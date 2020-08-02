b413011bd52a8917031f0b403d37e595346eefbe
ShauryaKaushik <65485931+ShauryaKaushik@users.noreply.github.com>
2020-08-02 11:56:38 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -89,11 +89,7 @@ function draw() {
      plinkos[i].display();
      
    }
-   if(frameCount%60===0){
-     particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
-     score++;
-   }
- 
+  
   for (var j = 0; j < particles.length; j++) {
    
      particles[j].display();
